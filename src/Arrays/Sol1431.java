2857050d8c28d92748aaaa746b1a2afc31e20181
David Huang <david.huang3214@gmail.com>
2023-03-01 16:26:18 -0800

    March1

---
+++ src/Arrays/Sol1431.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sol1431 {
+    public static void main(String[] args) {
+        int[] candies = {4,2,1,1,2};
+        int extraCandies = 1;
+        List<Boolean> ans = new ArrayList<Boolean>();
+        ans = kidsWithCandies(candies, extraCandies);
+        System.out.println(Arrays.toString(ans.toArray()));
+    }
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<Boolean>();
+        int max = 0;
+        int curr = 0;
+
+        // find maximum candies among children
+        for (int kid:
+             candies) {
+            if (max < kid){
+                max = kid;
+            }
+        }
+
+        // check greatest amount of candies among children
+        for (int i = 0; i < candies.length; i++) {
+            curr = candies[i] + extraCandies;
+
+            if (curr >= max){
+                ans.add(i,true);
+            }
+            else{
+                ans.add(i,false);
+            }
+            curr = 0;
+        }
+
+        return ans;
+    }
+}
