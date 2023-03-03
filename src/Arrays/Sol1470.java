fa6873f65377d0a9caaf89e4fec18a0436c65496
David Huang <david.huang3214@gmail.com>
2023-03-03 14:55:45 -0800

    March 3rd 2023

--- src/Arrays/Sol1470.java
+++ src/Arrays/Sol1470.java
@@ -1,10 +1,10 @@
-package leetcode.Arrays;
+package Arrays;
 import java.util.Arrays;
 
 public class Sol1470 {
     public static void main(String[] args) {
         int[] arr1 = {2,5,1,3,4,7};
-        int[] arr2 = {1,2,3,4,4,3,2,1};
+        //int[] arr2 = {1,2,3,4,4,3,2,1};
         int n = 3;
         int[] ans = shuffle(arr1, n);
         System.out.println(Arrays.toString(ans));
