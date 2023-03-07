d56419bcd8ce74e96e6b5cdb74c7c002b8bf91c2
David Huang <david.huang3214@gmail.com>
2023-03-06 17:56:12 -0800

    March 6th 2023

--- src/Searching/Notes2.java
+++ src/Searching/Notes2.java
@@ -5,12 +5,13 @@ public class Notes2 {
     public static void main(String[] args) {
         // note: array should be sorted before using binary search
         int[] arr = {2,3,5,9,14,16,18};
-        int target = 17;
-        int ans = findCeiling(arr,target);
-        System.out.println(ans);
-    }
+        int target = 1;
+        System.out.println("Target number: " + target);
+        System.out.println("Ceiling index is: " + findCeiling(arr,target));
+        System.out.println("Floor index is: " + findFloor(arr,target) + "\n");
 
-    public static int findCeiling(int[] arr, int target){
+    }
+    public static int findFloor(int[] arr, int target){
         int start = 0;
         int end = arr.length-1;
         while (start <= end){
@@ -25,7 +26,29 @@ public class Notes2 {
                 return mid;
             }
         }
-        return arr[start];
+        return end;
+
+    }
+    public static int findCeiling(int[] arr, int target){
+        if(target > arr[arr.length-1]){
+            return -1;
+        }
+
+        int start = 0;
+        int end = arr.length-1;
+        while (start <= end){
+            int mid = start + ((end-start)/2);
+            if(target < arr[mid]){
+                end = mid-1;
+            }
+            else if(target > arr[mid]){
+                start = mid+1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return start;
 
     }
     public static int binarySearch(int[] arr, int target){
