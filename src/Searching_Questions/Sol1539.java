2ea1b380504dec535fd5073a9247c5f7a4b2fbfe
David Huang <david.huang3214@gmail.com>
2023-03-12 22:39:38 -0700

    March 12th 2023

---
+++ src/Searching_Questions/Sol1539.java
@@ -0,0 +1,26 @@
+package Searching_Questions;
+
+public class Sol1539 {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,7,11};
+        int k = 5;
+        System.out.println(findKthPositive(arr, k));
+    }
+
+    public static int findKthPositive(int[] arr, int k) {
+        int start = 0;
+        int end = arr.length;
+
+        while(start < end){
+             int mid = start+((end-start)/2);
+            if(arr[mid] - mid > k){
+                end = mid;
+            }
+            else{
+                start = mid+1;
+            }
+
+        }
+        return start + k;
+    }
+}
