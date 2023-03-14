70802ffac810c0c7c2bf79b2037bb9c7ee7c6322
David Huang <david.huang3214@gmail.com>
2023-03-14 00:27:42 -0700

    March 13th 2023

---
+++ src/Strings_Questions/Sol1528.java
@@ -0,0 +1,20 @@
+package Strings_Questions;
+
+public class Sol1528 {
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = {4,5,6,7,0,2,1,3};
+        String ans = restoreString(s, indices);
+        System.out.println(ans);
+
+    }
+    public static String restoreString(String s, int[] indices) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            builder.
+
+        }
+
+    }
+
+}
