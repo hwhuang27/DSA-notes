448b6c63c46f4e35283c6e338e64d62c4de269f8
David Huang <david.huang3214@gmail.com>
2023-03-15 16:08:07 -0700

    March 14th 2023

--- src/Sorting_Notes/Notes3_InsertionSort.java
+++ src/Sorting_Notes/Notes3_InsertionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Notes3_InsertionSort {
     public static void main(String[] args) {
-        int[] arr = {5,4,3,2,1};
+        int[] arr = {4,5,6,7,0,2,1,3};
         insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
--- src/Strings_Questions/Sol1528.java
+++ src/Strings_Questions/Sol1528.java
@@ -1,35 +1,43 @@
 package Strings_Questions;
 
+import java.util.Arrays;
+
 public class Sol1528 {
     public static void main(String[] args) {
         String s = "codeleet";
         int[] indices = {4,5,6,7,0,2,1,3};
         String ans = restoreString(s, indices);
         System.out.println(ans);
+        System.out.println(Arrays.toString(indices));
 
     }
+
     public static String restoreString(String s, int[] indices) {
         char[] arr = s.toCharArray();
-        boolean swapped;
-        for (int i = 0; i < indices.length; i++) {
-            swapped = false;
-            for (int j = 1; j < indices.length-i; j++) {
-                if(indices[j] < indices[j-1]){
-                    int temp = indices[j-1];
-                    indices[j-1] = indices[j];
-                    indices[j] = temp;
-
-                    char temp2 = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp2;
-                    swapped = true;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if(indices[j] < indices[j-1]) {
+                    swap(indices, j, j-1);
+                    swap(arr, j, j-1);
                 }
-            }
-            if(!swapped){
-                return new String(arr);
+                else{
+                    break;
+                }
             }
         }
         return new String(arr);
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static void swap(char[] arr, int first, int second){
+        char temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 
 }
